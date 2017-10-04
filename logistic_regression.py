f12381d9adb1371b92186fe7b695d024e29e439b
Hugo Padovani <hgpadovani@gmail.com>
2017-10-04 17:24:42 -0300

    adding jupyter notebook

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,7 +2,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_predict                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+from scipy.stats import randint as sp_randint
 
 def training(classifier, X_train, y_train, X_test, y_test):
     """
@@ -56,6 +57,24 @@ def training(classifier, X_train, y_train, X_test, y_test):
              'y_pred_test': y_pred_test}
     return classifier, preds, metrics
 
+def get_gradients(model):
+    """Return the gradient of every trainable weight in model
+
+    Parameters
+    -----------
+    model : a keras model instance
+
+    First, find all tensors which are trainable in the model. Surprisingly,
+    `model.trainable_weights` will return tensors for which
+    trainable=False has been set on their layer (last time I checked), hence the extra check.
+    Next, get the gradients of the loss with respect to the weights.
+
+    """
+    weights = [tensor for tensor in model.trainable_weights if model.get_layer(tensor.name[:-2]).trainable]
+    optimizer = model.optimizer
+
+    return optimizer.get_gradients(model.total_loss, weights)
+
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -100,18 +119,34 @@ F, pval = f_regression(X_train, y_train)
 
 # Excluding features with low P value
 X_train = X_train[:, pval > 0.05]
-X_test = X_test[:, pval > 0.05]
+X_test = X_test[:, pval > 0.05 of .csv files and outputs an averaged submission. ]
 """
 # Fitting Logistic Regression to the training set
 from sklearn.linear_model import LogisticRegression
 
 # Model 1 - Logistic Regression One vs All
-clf_one_x_all = LogisticRegression(solver = 'sag', multi_class= 'ovr', random_state = 0, C=0.001, n_jobs=2)
+clf_one_x_all = LogisticRegression(solver = 'sag', multi_class= 'ovr', random_state = 0, C=1000)
 clf_one_x_all, preds_one_x_all, metrics_one_x_all2 = training(clf_one_x_all, X_train, y_train, X_test, y_test)
 
 # Model 2 - Logistic Regression Multinomial
 clf_multinom = LogisticRegression(solver = 'sag', multi_class= 'multinomial', random_state = 0, C=0.001, n_jobs=2)
 clf_multinom, preds_multinom, metrics_multinom = training(clf_multinom, X_train, y_train, X_test, y_test)
+
+# Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+parameters = {'C': [1,10,100,1000]                                                                              , 
+               'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag'],
+               }
+               
+             
+random_search = RandomizedSearchCV(estimator = clf_one_x_all,
+                           param_distributions = parameters,
+                           scoring = 'accuracy',
+                           n_iter = 10,
+                           n_jobs = 4)
+random_search = random_search.fit(X_train, y_train)
+best_accuracy = random_search.best_score_
+best_parameters = random_search.best_params_
 
 # Model 3 - Neural Nets
 # Importingthe Keras library and packages
@@ -120,11 +155,11 @@ from keras.layers import Dense, Dropout
 
 # Initialising the ANN and adding the layers
 clf_nn = Sequential()
-clf_nn.add(Dense(output_dim = 100, activation = 'relu', init='uniform', input_dim = 3072)) 
-clf_nn.add(Dropout(0.3))
-clf_nn.add(Dense(output_dim = 100, init = 'uniform', activation = 'relu')) # not necessary to inform the input dimension
-clf_nn.add(Dropout(0.3))
-clf_nn.add(Dense(output_dim = 10, init = 'uniform', activation = 'softmax')) # One output, using sigmoid for propabilistic outcome
+clf_nn.add(Dense(output_dim = 500, activation = 'relu', init='uniform', input_dim = 3072)) 
+clf_nn.add(Dropout(0.4))
+clf_nn.add(Dense(output_dim = 500,  actiitvation = 'relu')) # not necessary to inform the input dimension
+clf_nn.add(Dropout(0.4))
+clf_nn.add(Dense(output_dim = 10, activation = 'softmax')) # One output, using sigmoid for propabilistic outcome
 #clf_nn.add(Dropout(0.3))
 
 # Compiling the ANN
@@ -132,7 +167,7 @@ clf_nn.add(Dense(output_dim = 10, init = 'uniform', activation = 'softmax')) # O
 # loss = crossentropy for sigmoid function
 # metrics = tunned for best accuracy
 clf_nn.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 # creating dummies to the neural net
 from sklearn import preprocessing
 lb = preprocessing.LabelBinarizer()
@@ -140,7 +175,10 @@ y_train = lb.fit_transform(y_train)
 y_test = lb.fit_transform(y_test)
 
 # Training the neural net
-clf_nn.fit(X_train, y_train, batch_size = 200, epochs = 100)
+clf_nn.fit(X_train, y_train, batch_size = 400, epochs = 50)
+
+# Getting the numerical gradient
+grad = get_gradients(clf_nn)
 
 # Predicting the Test set results
 y_pred_nn = clf_nn.predict(X_test)
@@ -156,3 +194,7 @@ precision_test = precision_score(y_test, y_pred_nn, average = 'micro')
 recall_test = recall_score(y_test, y_pred_nn, average = 'micro')
 f1_test = f1_score(y_test, y_pred_nn, average = 'micro')
 cm_test = confusion_matrix(y_test, y_pred_nn)
+
+
+import seaborn as sns
+sns.heatmap(cm_test, annot=False, square=True)
